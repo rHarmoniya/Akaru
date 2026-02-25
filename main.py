901bf3e8267943c029fb074b54f7f78675292d98
Akaru <realyharmoniya@gmail.com>
2026-02-25 21:24:38 +0400

    base variables + practics

--- main.py
+++ main.py
@@ -1,19 +1,53 @@
-# This is a sample Python script.
+#variables
+#base variables: int, float, str, bool, None
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#integer and float differences
+integer_var = 1
+float_var = 1.5
 
+#2 types of string
+string_var = "Hello World"
+second_str_var = 'Hello Guys'
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#logical types(bools)
+true_bool_var = True
+false_bool_var = False
 
+#None type
+none_var = None
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#PRACTIC
+#Easy
+company_name = "Starbucks"
+current_price = 3.0
+volume = 5
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-1
-2
-3
+#Medium
+price = 100.0
+quantity = 6
+total_revenue = price * quantity
+print(total_revenue)
+
+data = 100
+data = "Сто"
+print(data)
+
+#Hard
+old_price = 120.0
+new_price = 150.0
+
+price_difference = new_price - old_price
+print(f"your price difference is {price_difference}")
+percent_difference = (price_difference / old_price) * 100
+print(f"your percent difference is {percent_difference}")
+
+#More
+start_price = 100.0
+demand = 15
+total_income = start_price * demand
+subsidy = None
+
+print(start_price)
+print(demand)
+print(total_income)
+print(subsidy)
