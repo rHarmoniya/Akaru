e0dfecb1a774fc064054a17df39fc034443c8e65
Arthur <arthurgishyan2006@gmail.com>
2026-03-02 21:38:31 +0400

    translated into Armenian

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 # #variables
-# #base variables: int, float, str, bool, None
+# #հիմնական փոփոխականների տեսակներ: int, float, str, bool, None
 #
-# #integer and float differences
+# #ամբողջ թվի և տասնորդական թվի տարբերությունը
 # integer_var = 1
 # float_var = 1.5
 #
-# #2 types of string
+# #տողի (string) 2 տեսակ
 # string_var = "Hello World"
 # second_str_var = 'Hello Guys'
 #
-# #logical types(bools)
+# #տրամաբանական տիպեր (bool)
 # true_bool_var = True
 # false_bool_var = False
 #
-# #None type
+# #None տիպ
 # none_var = None
 #
-# #PRACTIC
-# #Easy
+# #Վարժություններ
+# #Հեշտ
 # company_name = "Starbucks"
 # current_price = 3.0
 # volume = 5
 #
-# #Medium
+# #Միջին
 # price = 100.0
 # quantity = 6
 # total_revenue = price * quantity
@@ -32,7 +32,7 @@
 # data = "Сто"
 # print(data)
 #
-# #Hard
+# #Դժվար
 # old_price = 120.0
 # new_price = 150.0
 #
@@ -41,7 +41,7 @@
 # percent_difference = (price_difference / old_price) * 100
 # print(f"your percent difference is {percent_difference}")
 #
-# #More
+# #Ավելին
 # start_price = 100.0
 # demand = 15
 # total_income = start_price * demand
@@ -52,29 +52,29 @@
 # print(total_income)
 # print(subsidy)
 
-# #integer and float
+# #ամբողջ և տասնորդական թվեր
 # a = 15
 # b = 2
 # print(a//b)
 # print(a%b)
 # print(a**b)
 #
-# #string methods
+# #string մեթոդներ
 #
 # text1 = 'Hello World'
-# text2 = text1.strip()
+# text2 = text1.strip()  # հեռացնում է բացատները սկզբից և վերջից
 # print(text2)
-# text3 = text2.upper()
+# text3 = text2.upper()  # դարձնում է մեծատառ
 # print(text3)
-# text4 = text3.lower()
+# text4 = text3.lower()  # դարձնում է փոքրատառ
 # print(text4)
-# text5 = text2.replace(' ','-')
+# text5 = text2.replace(' ','-')  # փոխարինում է բացատները "-"
 # print(text5)
-# text6 = text2.split(sep='_')
+# text6 = text2.split(sep='_')  # բաժանում է "_" նշանով
 # print(text6)
 #
-# #None always = false
-# #Practics
+# #None-ը միշտ համարվում է False
+# #Վարժություններ
 # number = 25
 # print(number**0.5)
 #
@@ -84,7 +84,7 @@
 # my_num = 42
 # print(my_num%2)
 #
-# #Medium
+# #Միջին
 # x = 0.1
 # y = 0.2
 # print(x+y)
@@ -93,101 +93,100 @@
 # secret_key = None
 # print(secret_key is None)
 #
-# #Hard
+# #Դժվար
 # db_record = "user_email@GMAIL.com \n"
 # clean_record = db_record.strip().lower()
 # print(clean_record)
 #
-# #More
+# #Ավելին
 # file_name = "   RePort_2026.TXT"
 # file_size = 1048576
 # file_name_fix = file_name.strip().lower().replace(".txt",".md")
 # file_size_gb = file_size//(1024**2)
 # print(f"File {file_name_fix} has {file_size_gb} GB")
 
-#Collections(list, tuple, slicing)
-#1 list and list methods
+# Collections(list, tuple, slicing)
+# 1 list և list մեթոդներ
 # cars = ['BMW', 'Mercedess', 'Audi']
-# cars.append('Reno') #ADD element to the end
+# cars.append('Reno') # ավելացնում է տարր վերջում
 # print(cars)
-# #element[n:N] take frome n to N-1 elements
+# # element[n:N] վերցնում է n-ից մինչև N-1 տարրերը
 # print(cars[1:3])
-# print(cars[::-1]) #::-1 reverse list
+# print(cars[::-1]) # շրջում է ցուցակը (reverse)
 #
-# #tuple
-# local_host = ("localhost", 8080, "production") #we can`t change something after creation
+# # tuple
+# local_host = ("localhost", 8080, "production") # ստեղծելուց հետո չի կարելի փոխել
 # print(local_host)
 
-#PRACTICS
-# #Easy
+# Վարժություններ
+# #Հեշտ
 # linux_distros = ['Ubuntu', 'Debian', 'Fedora']
 # print(linux_distros[1])
 #
 # my_tuple = (1, 2, 3)
 # print(my_tuple[-1])
 #
-# #Medium
+# #Միջին
 # prices = [120, 150, 130, 180, 200]
 # print(prices[0:3])
 # print(prices[::-1])
 #
-# #Hard
+# #Դժվար
 # raw_data = "Python|JS|HTML|CSS"
 # tech_list = raw_data.split("|")
 # tech_list.append("SQL")
 # print(tech_list)
 #
-# #More
+# #Ավելին
 # portfolio = ["Apple", "Tesla", "Google", "Amazon"]
 # portfolio[1] = "Microsoft"
 # portfolio.append("Nvidia")
 # print(portfolio)
 # print(portfolio[1:3])
 #
-
-#list methods
-#append
-my_list = [1,2,3,4]
-my_list.append(5) #add 5 to the end
-print(my_list)
-
-#insert
-my_list.insert(5, 'Bash')
-print(my_list)
-
-#extend
-second_list = ['Terminal', 'Powershell']
-my_list.extend(second_list)# join 2 lists in 1
-print(my_list)
-
-#remove
-price_list = [150,150.3,150,168]
-price_list.remove(150) #while we use remove(x) it removes the first x, but  not all
-print(price_list)
-
-#pop
-word_list = ['First Word', 'Second Word', 'Third Word']
-word_list.pop(0) #using pop we remove the element by index, if we dont write index, it pop the last el
-print(word_list)
-
-#clear
-word_list.clear() #it fully clear all elements
-print(word_list)
-
-#index
-votes = ["yes", "no", "yes", "no", "abstain"]
-no_index = votes.index('no') #it return the first finded index
-print(no_index)
-
-#count
-yes_count = votes.count('yes') #it counts the elements
-print(yes_count)
-
-#sort
-unsorted_list = ['BingOS', 'Android', 'Google', 'Ios']
-unsorted_list.sort()
-print(unsorted_list)
-
-#reverse
-unsorted_list.reverse()
-print(unsorted_list)
+# list մեթոդներ
+# append
+# my_list = [1,2,3,4]
+# my_list.append(5) # ավելացնում է 5 վերջում
+# print(my_list)
+#
+# insert
+# my_list.insert(5, 'Bash') # ավելացնում է տարր կոնկրետ ինդեքսում
+# print(my_list)
+#
+# extend
+# second_list = ['Terminal', 'Powershell']
+# my_list.extend(second_list) # միացնում է 2 ցուցակ
+# print(my_list)
+#
+# remove
+# price_list = [150,150.3,150,168]
+# price_list.remove(150) # հեռացնում է առաջին հանդիպած 150-ը
+# print(price_list)
+#
+# pop
+# word_list = ['First Word', 'Second Word', 'Third Word']
+# word_list.pop(0) # հեռացնում է տարրը ըստ ինդեքսի, եթե ինդեքս չկա՝ վերջին տարրը
+# print(word_list)
+#
+# clear
+# word_list.clear() # ամբողջությամբ մաքրում է ցուցակը
+# print(word_list)
+#
+# index
+# votes = ["yes", "no", "yes", "no", "abstain"]
+# no_index = votes.index('no') # վերադարձնում է առաջին գտնված ինդեքսը
+# print(no_index)
+#
+# count
+# yes_count = votes.count('yes') # հաշվում է տարրերի քանակը
+# print(yes_count)
+#
+# sort
+# unsorted_list = ['BingOS', 'Android', 'Google', 'Ios']
+# unsorted_list.sort() # դասավորում է ցուցակը
+# print(unsorted_list)
+#
+# reverse
+# unsorted_list.reverse() # շրջում է ցուցակը
+# print(unsorted_list)
